2394a6607067081b41f2322c89e7f95036170033
Sachin M Sibichan <sachinm06@aol.com>
2023-02-18 14:03:12 +0530

    new changes

--- script.js
+++ script.js
@@ -1,6 +1,16 @@
 function loginBtn(){
+
+    uname=usern.value
+    localStorage.setItem("user",uname)
+
+
+
     window.location="home.html"
 }
 function logOut(){
+    localStorage.removeItem("user")
     window.location="index.html"
 }
+
+userna=localStorage.getItem("user")
+d1.innerHTML=  userna
