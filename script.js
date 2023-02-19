896f4d730ecf637a3f63d1805440b72b74b7fdce
Sachin M Sibichan <sachinm06@aol.com>
2023-02-19 13:41:49 +0530

    new change

--- script.js
+++ script.js
@@ -1,21 +1,60 @@
-function loginBtn(){
+function loginBtn() {
 
-    uname=usern.value
-    localStorage.setItem("user",uname)
+    uname = usern.value
+    localStorage.setItem("user", uname)
 
 
 
-    window.location="home.html"
+    window.location = "home.html"
 }
-function logOut(){
+function logOut() {
 
     localStorage.removeItem("user")
-    window.location="index.html"
+    window.location = "index.html"
 }
 
-userna=localStorage.getItem("user")
-d1.innerHTML= userna
+userna = localStorage.getItem("user")
+d1.innerHTML = userna
 
-function viewItem(){
-    window.location="result.html"
+function viewItem() {
+    window.location = "result.html"
+    localStorage.getItem()
+}
+function resultLogout() {
+    localStorage.removeItem("user")
+    window.location = "index.html"
+}
+
+function addBtn() {
+    product = {
+        pName: pName.value, ID: pId.value,
+        price: pPrice.value,
+        Quantity: pQnty.value
+    }
+
+    if (product.ID in localStorage) {
+        alert("Product already exist")
+    }
+    else {
+        localStorage.setItem(product.ID, JSON.stringify(product))
+        alert("New Product Added")
+    }
+
+}
+function searchBtn() {
+    res = resultId.value
+    if (res in localStorage) {
+        prod = JSON.parse(localStorage.getItem(res))
+        //  console.log(prod);
+        viewResult.innerHTML = `
+<h3>Product Name &nbsp &nbsp &nbsp: ${prod.pName}</h3>
+<h3>Product ID &nbsp &nbsp &nbsp&nbsp&nbsp&nbsp &nbsp&nbsp &nbsp: ${prod.ID}</h3>
+<h3>Product Price &nbsp &nbsp&nbsp &nbsp&nbsp: ${prod.price}</h3>
+<h3>Product Quantity : ${prod.Quantity}</h3>
+`
+    }
+    else {
+        viewResult.innerHTML =" "
+        alert("Product not found")
+    }
 }
